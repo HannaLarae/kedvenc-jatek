4b99b14861e2cc3f7ec1dbd3aff1c3a2b9068699
BettiMacsek <116814010+BettiMacsek@users.noreply.github.com>
2024-03-20 13:51:05 +0100

    Add files via upload

--- bettipython.py
+++ bettipython.py
@@ -1,3 +1,4 @@
+
 jatekertekek=[]
 f=open("játékok.txt", "r", encoding="utf-8")
 for sor in f:
@@ -14,5 +15,13 @@ print("1. megoldás: Összesen: "+str(jatekertekek)+" szó található az össze
 
 print("Az átlagpontszám: "+str(átlagpontszám))
 print("Fogok kérni egy pontértéket. Az ennél nagyobb pontszámot elért játékok nevét begyűjtöm egy fájlba, a fájl neve jatékok_pont.txt")
-Pontérték=input("Adja meg a pontértéket!")
+pontérték=int(input("Adja meg a pontértéket!"))
 
+g=open("játékok_pont.txt", "w", encoding="utf-8")
+for i in range(0, len(jatekertekek)):
+    if int(jatekertekek[i][1])>pontérték:
+        g.write(jatekertekek[i][1])
+        g.write("\n")
+
+g.close()
+#Írja ki a legtöbb és a legkevesebb pontot kapó játék nevét és pontszámát a képernyőre.
